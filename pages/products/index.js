f63ec618ee6c751a3103b076df683bc0ce2daa49
SanjaMishra <sanjmish1987@gmail.com>
2022-11-29 17:44:24 +0530

    isr

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,33 +1,111 @@
-function ProductList({ products }) {
+// function ProductList({ products }) {
+//     return (
+//       <>
+//         <h1>List of products</h1>
+//         {products.map(product => {
+//           return (
+//             <div key={product.price}>
+//               <h2>
+//                 {product.id} {product.title} {product.price}
+//               </h2>
+//               <hr />
+//             </div>
+//           )
+//         })}
+//       </>
+//     )
+//   }
+  
+//   export default ProductList
+  
+//   export async function getStaticProps() {
+//     console.log('Generating / Regenerating ProductList')
+//     const response = await fetch('http://jsonplaceholder.typicode.com/users')
+//     const data = await response.json()
+  
+//     return {
+//       props: {
+//         products: data
+//       },
+//       revalidate: 30
+//     }
+//   }
+  
+
+
+import Footer from '../components/header'
+import Header from '../components/footer'
+import Link from 'next/link'
+const UsersPage = (props) => {
     return (
-      <>
-        <h1>List of products</h1>
-        {products.map(product => {
-          return (
-            <div key={product.id}>
-              <h2>
-                {product.id} {product.title} {product.price}
-              </h2>
-              <hr />
+        <div>
+            <Header></Header>
+
+            <div className='container col-10 mt-2 '>
+                <Link href="/">
+                    <span className='btn btn-dark mt-2'>Back to Home page</span>
+                </Link>
+                <div className="jumbotron mb-2 mt-4">
+                    <h4>Incremental Static Regeneration</h4>
+                    
+                </div>
+                   
+
+ 
+            <div className='custom-table mb-5 mb-2'>
+                <table className='  table-striped  table table-bordered text-center col-5'>
+                <thead className="thead-dark">
+                    <tr className="bg-info">
+                        <th>User ID</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Age</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.data.users.map((user) => (
+                            <tr>
+                                <td>
+                                    <Link href={`users/${user.id}`} key={user.id}>
+                                      
+                                    <button type="button" class="btn btn-dark  btn-sm">{user.id}</button>   
+                                    </Link>
+                                </td>
+                                <td>  {user.firstName}</td>
+                                <td>  {user.lastName}</td>
+                                <td>  {user.age}</td>
+                                <td>  {user.email}</td>
+
+                            </tr>
+                        ))}
+                </tbody>
+            </table>
             </div>
-          )
-        })}
-      </>
+                {/* {props.data.users.map(user =>
+                    <li key={user.id}>{user.firstName} {user.lastName}</li>
+                )
+                }  */}
+                 
+                </div>
+                <br/><br/>
+            <Footer></Footer>
+        </div>
     )
-  }
-  
-  export default ProductList
-  
-  export async function getStaticProps() {
-    console.log('Generating / Regenerating ProductList')
-    const response = await fetch('http://jsonplaceholder.typicode.com/users')
-    const data = await response.json()
-  
+}
+
+export const getStaticProps = async () => {
+     const data = await (await fetch("http://dummyjson.com/users")).json()
     return {
-      props: {
-        products: data
-      },
-      revalidate: 30
-    }
-  }
-  
+        props: {
+            data,
+        },
+    };
+}
+export default UsersPage;
+
+
+
+
+
+ 
--- pages/server/users.js
+++ pages/server/users.js
@@ -7,36 +7,20 @@ const UserPage = (props) => {
 
     <div>
       <header className="header-container">
-        <img src='https://www.ust.com/content/dam/ust/images/logos/ust-white-logo.svg' alt='logo' />
+      <h5 className='text-white'>Logo</h5> 
       </header>
-      <div className='container-sm col-8 mt-5'>
+      <div className='container-sm col-10 mt-1'>
 
-        <Link href="/">
-          Back to Home page
-        </Link>
+      <Link href="/">
+                    <span className='btn btn-dark mt-2'>Back to Home page</span>
+                </Link>
         {/* <div >{JSON.stringify(Users)} </div>  */}
 
         <div className="jumbotron mb-5">
 
           <h3 className="mt-3">SSR Server-side Rendering</h3>
-          {/* <p> Server-side rendering means using a server to generate HTML from JavaScript modules in response to a URL request. That’s in contrast to client-side rendering, which uses the browser to create HTML using the DOM.
-          </p> */}
-          {/* <h4>Pros</h4>
-          <ul>
-            <li>It enables pages to load faster which provides a better user experience.</li>
-            <li>It plays an important role in SEO (search engine optimization) and correctly indexes webpages. This happens because Google favors web pages with faster load time.</li>
-            <li>It provides body to the HTML pages for all server ships.</li>
-          </ul>
-          <h4>cons</h4>
-          <ul>
-            <li>Slower page transitions</li>
-            <li><b>Vulnerability:</b> SSR sites are harder to keep secure because they have a bigger surface to attack than CSR sites. This is however not an issue if you or your developers know what they’re doing.</li>
-            <li><b>Complex caching: </b>configuring your cache is usually more complex on SSR sites than CSR sites.</li>
-            <li>Server cost: </li>
-
-          </ul> */}
-          {/* <h4>Explanation</h4>
-          <p><b>Why we need server side rendering, </b>First thing it is fast. In SSR user cannot see the data(Api keys , Headers) in the NetworkTabwhere as in the Cleint side rendering, user can see the data. So evering thing is exposed in the network tab when we are working on the CSR. In SSR it is not possible. </p> */}
+    
+          
           <div className="alert alert-primary" role="alert">
             <b>export async function getServerSideProps(){<br />
             }</b>
@@ -53,7 +37,7 @@ const UserPage = (props) => {
                     )
              } */}
 
-      <div className='container col-8 custom-table'>
+      <div className='container col-10 '>
         <table className=' table-striped table table-bordered text-center '>
           <thead className="thead-dark">
             <tr className="bg-info">
