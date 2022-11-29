7b29ba42b79b763300ecff3b71f876c888382a01
SanjaMishra <sanjmish1987@gmail.com>
2022-11-29 14:59:09 +0530

    Next commit

--- pages/404.js
+++ pages/404.js
@@ -5,9 +5,9 @@
    return (
      <div>
        <Header></Header>
-       <div class="container">
-    <div class="row align-items-center">
-        <div class="col-md-12"> Not Found 404 ------------------
+       <div className="container">
+    <div className="row align-items-center">
+        <div className="col-md-12"> Not Found 404 ------------------
         <img src="https://img.freepik.com/free-vector/error-404-concept-landing-page_52683-12757.jpg?w=900&t=st=1669363827~exp=1669364427~hmac=efc2f85eab3fd1d048431010206fb0f72bd2379561547e0f45865ebab5f3b7e6"></img>
         </div>
     </div>
